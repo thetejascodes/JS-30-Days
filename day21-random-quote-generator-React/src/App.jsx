ca7405e5703b4463bf78b7f8c50e09747e3e1648
tejaskale99 <tejaskale1556@gmail.com>
2025-07-20 15:32:22 +0530

    Added Day 21 Random Quote Generator App using (React).

---
+++ day21-random-quote-generator-React/src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import './App.css'
+
+const App = () => {
+  const quotes = [
+    "Success is not final; failure is not fatal.",
+    "Stay hungry, stay foolish.",
+    "Your time is limited, don't waste it living someone else's life.",
+    "Push yourself, because no one else is going to do it for you.",
+  ];
+  const [quote,setQuote] = useState("Get Random Quote");
+  const getQuote = ()=>{
+     const randomNumber = Math.floor(Math.random()*quotes.length);
+    setQuote(quotes[randomNumber]);
+    console.log(quote)
+  }
+  return (
+    <div className="app">
+      <h2>{quote}</h2>
+     <button onClick={getQuote}>Get New Quote</button>
+    </div>
+  );
+};
+
+export default App;
