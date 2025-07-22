a2d779d87c4c745d544132b21bdba0575e7941ae
tejaskale99 <tejaskale1556@gmail.com>
2025-07-22 16:41:49 +0530

    Added homepage for github

--- day21-random-quote-generator-React/vite.config.js
+++ day21-random-quote-generator-React/vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+   plugins: [react()],
   base: '/JS-30-Days/day21-random-quote-generator-React/',
-  plugins: [react()],
 })
