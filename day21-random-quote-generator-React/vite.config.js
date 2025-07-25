b660ac87a0564c4dba4d3356e6b426842a059871
tejaskale99 <tejaskale1556@gmail.com>
2025-07-25 13:18:25 +0530

    Trying to solve error and deploy again

--- day21-random-quote-generator-React/vite.config.js
+++ day21-random-quote-generator-React/vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-   plugins: [react()],
+  plugins: [react()],
   base: '/JS-30-Days/day21-random-quote-generator-React/',
 })
